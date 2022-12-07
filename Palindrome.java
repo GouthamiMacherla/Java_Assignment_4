c99cd8f19365587a34931e3c9a389e67d494c0d4
GouthamiMacherla <115568506+GouthamiMacherla@users.noreply.github.com>
2022-12-07 13:05:14 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,34 @@
+/*
+ * @Gouthami Macherla
+ * 
+ * Program to find if the given number is a Palindrome
+ * 
+ */
+
+
+public class Palindrome
+{
+	public static void main(String[] args) 
+	{
+		int r,temp;
+		int sum=0;
+		int n=2552;
+		System.out.println("Given palindrome number is::"  +n);
+		temp=n;
+		while(n>0)
+		{
+			r=n%10;
+			sum=(sum*10)+r;
+			n=n/10;
+		}
+		if(temp==sum)
+		{
+			System.out.println("Number is Palindrome");
+		}
+		else
+		{
+			System.out.println("number is not Palindrome");
+		}
+	}
+
+}
